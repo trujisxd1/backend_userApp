7af25e485795205f9bcbc783b42648a5bb4b9b59
Gustavo M <gutamvol262@gmail.com>
2023-10-30 13:07:16 -0600

    configuracion de spring security protegiendo las rutas de post y delete asi como implementar lass clases de encriptador

--- src/main/java/com/gustavo/backend/userapp/backenduserapp/auth/SpringSecurityConfig.java
+++ src/main/java/com/gustavo/backend/userapp/backenduserapp/auth/SpringSecurityConfig.java
@@ -1,23 +1,57 @@
 package com.gustavo.backend.userapp.backenduserapp.auth;
 
+import com.gustavo.backend.userapp.backenduserapp.auth.filters.JwtAuthenticationFilter;
+import com.gustavo.backend.userapp.backenduserapp.auth.filters.JwtValidationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
 
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws  Exception {
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
 
-        return http.authorizeHttpRequests().requestMatchers(HttpMethod.GET,"/api").permitAll().anyRequest()
+    @Bean
+        //cadena de filtro para autorizacion
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+
+        return http.authorizeHttpRequests()
+                // Autorización para el método GET en la URL /api (cualquiera puede acceder)
+                .requestMatchers(HttpMethod.GET, "/api").permitAll()
+                // Todas las demás solicitudes requieren autenticación
+                .anyRequest()
                 .authenticated()
                 .and()
+                // Agrega el filtro de autenticación JWT
+                .addFilter(new JwtAuthenticationFilter(authenticationConfiguration.getAuthenticationManager()))
+                // Deshabilita la protección CSRF
+                .addFilter(new JwtValidationFilter(authenticationConfiguration.getAuthenticationManager()))
                 .csrf(config -> config.disable())
+                // Establece la gestión de sesiones en Stateless (sin sesiones)
                 .sessionManagement(managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
     }
 
+    @Bean
+    ///enxriptador de contrasñas
+    PasswordEncoder passwordEncoder (){
+
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    AuthenticationManager authenticationManager() throws  Exception{
+
+        return  authenticationConfiguration.getAuthenticationManager();
+    }
 }
