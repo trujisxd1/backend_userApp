865b921aee565eb6593d8d19be0500c417809b6e
Gustavo M <gutamvol262@gmail.com>
2023-10-30 13:04:09 -0600

    creamos la consulta para el inicio de session y traer el username

--- src/main/java/com/gustavo/backend/userapp/backenduserapp/repository/UserRepository.java
+++ src/main/java/com/gustavo/backend/userapp/backenduserapp/repository/UserRepository.java
@@ -2,7 +2,15 @@ package com.gustavo.backend.userapp.backenduserapp.repository;
 
 import com.gustavo.backend.userapp.backenduserapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserRepository  extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+    @Query("select u from User u where  u.username=?1")
+    Optional<User> getUserByUsername(String username);
+
 
 }
